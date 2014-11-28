c358988869172aa22a98446340ab68438c9c9562
James Reeves <jreeves@weavejester.com>
2014-11-28 19:36:44 +0000

    Initial commit

---
+++ src/ring/middleware/webjars.clj
@@ -0,0 +1,6 @@
+(ns ring.middleware.webjars)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ring/middleware/webjars_test.clj
@@ -0,0 +1,7 @@
+(ns ring.middleware.webjars-test
+  (:require [clojure.test :refer :all]
+            [ring.middleware.webjars :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
