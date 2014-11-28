f6ca3218c0d02548281daea6d22606401d527ef2
James Reeves <jreeves@weavejester.com>
2014-11-28 22:49:08 +0000

    Add wrap-webjars middleware function

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.webjars/webjars-locator "0.19"]
+                 [org.slf4j/slf4j-nop "1.7.7"]
+                 [ring/ring-core "1.3.2"]]
+  :profiles
+  {:dev {:dependencies [[org.webjars/bootstrap "3.3.1"]
+                        [ring-mock "0.1.5"]]}})
