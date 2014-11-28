76877f8ca8b94787baee1521b65cc3fe448b8470
James Reeves <jreeves@weavejester.com>
2014-11-28 22:49:08 +0000

    Add wrap-webjars middleware function

--- src/ring/middleware/webjars.clj
+++ src/ring/middleware/webjars.clj
@@ -1,6 +1,34 @@
-(ns ring.middleware.webjars)
+(ns ring.middleware.webjars
+  (:import [org.webjars WebJarAssetLocator])
+  (:require [ring.middleware.head :as head]
+            [ring.util.codec :as codec]
+            [ring.util.request :as req]
+            [ring.util.response :as resp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:private webjars-pattern
+  #"META-INF/resources/webjars/([^/]+)/([^/]+)/(.*)")
+
+(defn- asset-path [prefix resource]
+  (let [[_ name version path] (re-matches webjars-pattern resource)]
+    (str prefix "/" name "/" path)))
+
+(defn- asset-map [^WebJarAssetLocator locator prefix]
+  (->> (.listAssets locator "")
+       (map (juxt (partial asset-path prefix) identity))
+       (into {})))
+
+(defn- request-path [request]
+  (codec/url-decode (req/path-info request)))
+
+(defn wrap-webjars
+  ([handler]
+     (wrap-webjars handler "/assets"))
+  ([handler prefix]
+     (let [assets (asset-map (WebJarAssetLocator.) prefix)]
+       (fn [request]
+         (if (#{:head :get} (:request-method request))
+           (if-let [path (assets (request-path request))]
+             (-> (resp/resource-response path)
+                 (head/head-response request))
+             (handler request))
+           (handler request))))))
--- test/ring/middleware/webjars_test.clj
+++ test/ring/middleware/webjars_test.clj
@@ -1,7 +1,19 @@
 (ns ring.middleware.webjars-test
   (:require [clojure.test :refer :all]
-            [ring.middleware.webjars :refer :all]))
+            [clojure.java.io :as io]
+            [ring.middleware.webjars :refer :all]
+            [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn- slurp-response [handler uri]
+  (some-> (mock/request :get uri) handler :body slurp))
+
+(defn- slurp-webjars [path]
+  (slurp (io/resource (str "META-INF/resources/webjars/" path))))
+
+(deftest test-wrap-webjars
+  (let [handler (wrap-webjars (constantly nil))]
+    (is (nil? (handler (mock/request :get "/foo"))))
+    (is (nil? (handler (mock/request :get "/assets"))))
+    (is (nil? (handler (mock/request :get "/assets/bootstrap"))))
+    (is (= (slurp-response handler "/assets/bootstrap/less/close.less")
+           (slurp-webjars "bootstrap/3.3.1/less/close.less")))))
